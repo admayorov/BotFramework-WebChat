d55d2ef5dccac589fea0c3f4f64ab6461ea37a54
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-10-31 22:43:35 +0000

    Deploy Microsoft/BotFramework-WebChat to github.com/Microsoft/BotFramework-WebChat.git:gh-pages

---
+++ breakaway-speech-button/precache-manifest.af4f9374187b0ba52d5637c186503688.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d7936e5b16c017c22e0c",
+    "url": "/BotFramework-WebChat/breakaway-speech-button/static/js/runtime~main.d7936e5b.js"
+  },
+  {
+    "revision": "8b2bfc8bc46c5b3e785c",
+    "url": "/BotFramework-WebChat/breakaway-speech-button/static/js/main.8b2bfc8b.chunk.js"
+  },
+  {
+    "revision": "b715a5419920932d8109",
+    "url": "/BotFramework-WebChat/breakaway-speech-button/static/js/1.b715a541.chunk.js"
+  },
+  {
+    "revision": "8b2bfc8bc46c5b3e785c",
+    "url": "/BotFramework-WebChat/breakaway-speech-button/static/css/main.9ea24ea0.chunk.css"
+  },
+  {
+    "revision": "4379240682019b41b898236d0881e7f7",
+    "url": "/BotFramework-WebChat/breakaway-speech-button/index.html"
+  }
+];
